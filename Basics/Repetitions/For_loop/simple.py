02a3d380879ede0fb8324a5727633883a5ee4d84
repl.it user <replituser@example.com>
2020-10-06 11:44:18 +0000

    Added code to demonstrate the use of a simple while loop.

---
+++ Basics/Repetitions/For_loop/simple.py
@@ -0,0 +1,12 @@
+print("How many mountains should I display?")
+amount = input()
+
+for (amount):
+  print("      __    ")
+  print("     /  \_    ")
+  print("    /   ^ \  ")
+  print("   /  ^    \_")
+  print(" _/          \")
+  print("/    ^ ^      \")
+  
+print("Done!")
---
+++ Basics/Repetitions/While_loop/simple.py
@@ -0,0 +1,8 @@
+print("How many cables should i remove?")
+cables = int(input())
+
+removed = 0
+
+while (removed < cables):
+  print("Removed Cable")
+  removed += 1
