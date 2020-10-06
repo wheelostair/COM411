cb61c552f5922322b20ed5293bb639678fb45174
repl.it user <replituser@example.com>
2020-10-06 09:16:51 +0000

    Added code to demonstrate the use of an if...else statement.

---
+++ Basics/Decisions/Simple_decision/if_else.py
@@ -0,0 +1,11 @@
+#Ask for user input
+print("Please enter the activity to be performed.")
+activity = input()
+#if input = calculate
+if (activity == "calculate"):
+  print("Performing calculations...")
+#if input = anything else  
+else:
+  print("Performing activity...")
+
+print("Activity completed!")
