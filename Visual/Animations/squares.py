d788d55f1a6ab41fbc9fd7c6bc9b6bb464c24b81
repl.it user <replituser@example.com>
2020-11-24 13:10:51 +0000

    made adjustments to lesson

--- Visual/Animations/squares.py
+++ Visual/Animations/squares.py
@@ -6,27 +6,23 @@ boxes = []
 fig, ax = plt.subplots()
 
 def init():
-  small = {"x":(3,4,4,3,3),"y":(3,3,4,4,3)}
-  medium = {"x":(2,5,5,2,2),"y": (2,2,5,5,2)}
-  large = {"x":(1,6,6,1,1),"y": (1,1,6,6,1)}
+  boxes.append({"x":[3,4,4,3,3],"y":[3,3,4,4,3]})
+  boxes.append({"x":[2,5,5,2,2],"y": [2,2,5,5,2]})
+  boxes.append({"x":[1,6,6,1,1],"y": [1,1,6,6,1]})
 
-  boxes.append(small)
-  boxes.append(medium)
-  boxes.append(large)
-  
   return (boxes)
 
 def animate(frame):
-  data = init()
-  x = data[0]
-  y = data[1]
-
-  print (data)
-  ax.plot(x, y)
+  global ax
+  index = frame % len(boxes)
+  ax.cla()
+  ax.set_xlim(0, 7)
+  ax.set_ylim(0, 7)
+  ax.plot(boxes[index]["x"],boxes[index]["y"])
 
 def run():
   global fig  
-  simple_animation = animation.FuncAnimation(fig,animate, frames = 10, interval = 1000)
+  simple_animation = animation.FuncAnimation(fig,animate, frames = 100, interval = 100, init_func=init)
 
   plt.show()
 
